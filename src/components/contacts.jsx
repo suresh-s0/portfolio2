a7b0f3ffe312369706d97a1d5be157ae833f40d6
suresh-sterna <sureshs@sternadevices.in>
2024-11-07 16:58:30 +0530

    added contact section

---
+++ src/components/contacts.jsx
@@ -0,0 +1,19 @@
+function Contacts(){
+    return(
+        <div className="flex justify-center items-center">
+            <button>
+                <a href="mailto:sureshaster09@gmail.com" className="text-white p-1 mr-1">
+               mail
+                </a>               
+            </button>
+          
+            <a href="https://www.linkedin.com/in/suresh-s0/" target="_blank" className="text-white p-1 mr-1">
+                    <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="suresh-s003" height="10" width="20" /></a>
+
+                <a href="https://github.com/suresh-s0" target="_blank" className="text-white p-1 mr-1">
+                    <img align="center" src="https://github.githubassets.com/favicons/favicon-dark.svg" alt="suresh-s0" height="6" width="20" /></a>
+        </div>
+    )
+}
+
+export default Contacts;
