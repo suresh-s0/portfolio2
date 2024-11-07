e1cacedf8bf23efda6e9cf5d8905ab0e51a3dc81
suresh-sterna <sureshs@sternadevices.in>
2024-11-07 16:58:30 +0530

    added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import Topbar from './components/topbar'
 import Home from './components/home'
 import About from './components/about'
 import Projects from './components/projects'
-
+import Contacts from './components/contacts'
 
 function App() {
 
   return (
-    <div className="bg-gradient-to-r from-gray-800 via-black to-gray-800 font-mono ">
+    <div className="bg-gradient-to-r from-gray-800 via-black to-gray-800 font-mono scrollable-container">
       <div >
 
         <Topbar></Topbar>
         <Home></Home>
         <About></About>
         <Projects></Projects>
+        <Contacts></Contacts>
 
       </div>
 
