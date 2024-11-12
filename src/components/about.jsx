a0767092e55d160b89dc2a7e36b4eb6996f2897c
Suresh-s5 <sureshaster09@gmail.com>
2024-11-12 21:18:49 +0530

    designed about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,35 +1,39 @@
 function About() {
 
-    return (           
-         <div className="flex flex-col justify-center items-center mt-5  border-b-4 border-gray-800 ">
-            <h1 className="text-white">About me</h1>
-            <div className="flex w-[50%] mt-8">
-               
-                <h2 className="text-white">
-                    Hello! I'm an up-and-coming Full Stack Developer .
-                    With a solid foundation in both front-end and back-end technologies, I'm passionate about crafting 
-                    dynamic and responsive web applications that deliver seamless user experiences.
-                </h2>
+    return (
+        <div className="flex mt-5  border-r-4 border-gray-800 px-20">
+            <div className="border-x-2 border-gray-800 w-[50%] flex flex-col justify-center items-center">
+
+                <h1 className="text-white">About me</h1>
+                <div className="flex mt-8">
+
+                    <h2 className="text-white text-wrap">
+                        Hello! I'm an up-and-coming Full Stack Developer .
+                        With a solid foundation in both front-end and back-end technologies, I'm passionate about crafting
+                        dynamic and responsive web applications that deliver seamless user experiences.
+                    </h2>
+                </div>
+
             </div>
 
 
-
-            <div className="text-white flex flex-col w-[50%] justify-center border  py-8">
+            <div className="text-white flex flex-col  w-[50%] px-4 py-8">
                 <h1>Experience:</h1>
                 <div className="px-6 py-3">
                     <h2>Company: Sterna Devices</h2>
                     <h2>Role: Junior Software Developer</h2>
                     <h2>Duration: Apirl-2024 to present </h2>
                 </div>
-                
+
                 <h1>Responsibilities:</h1>
                 <div className="px-6 py-2">
                     <h2 className="py-2">** Working on projects involving GO,Gin,Gorm and  SQL to develop and maintain applications.</h2>
                     <h2 className="py-2">** Collaborated with cross-functional teams to implement  user authentication, API integrations AND CRUD functions.</h2>
-                    
+
                 </div>
             </div>
-            </div>
+
+        </div>
     )
 }
 
